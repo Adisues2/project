72a9d65ee2546d3585b30e75eb0dde6dc77d782f
adisues2 <eshetu.adisu21@gmail.com>
2021-07-24 13:13:27 +0300

    javascript excercise will be added

--- javascript2021/week_3.js
+++ javascript2021/week_3.js
@@ -1,106 +1,139 @@
-console.log("hello world");
-console.log("welcom to javascript");
-let str = "Hello Everyone, please say hello to the class";
-let posUppercaseHello = str.indexOf("Hello"); // case sensitive
-let posLowercaseHello = str.indexOf("hello");
-console.log(posUppercaseHello) // 0
-console.log(posLowercaseHello) // 27
+// console.log("hello world");
+// console.log("welcom to javascript");
+// let str = "Hello Everyone, please say hello to the class";
+// let posUppercaseHello = str.indexOf("Hello"); // case sensitive
+// let posLowercaseHello = str.indexOf("hello");
+// console.log(posUppercaseHello) // 0
+// console.log(posLowercaseHello) // 27
 
-let secondstr = "hello Everyone, please say hello to the class";
-let secondStrPosHello = secondstr.indexOf("hello"); 
-console.log(secondStrPosHello) // 0
-let adisu = "wow";
-let adisus = "hey";
-console.log(adisu);
-
-
-let text = "welcome \weldone\  weljob";
-console.log(text);
-let Text = "Hello Everyone please say hello";
-console.log(Text.substring(0,2));
-
-let addressNumber = 45;
-let addressStreet = "allenby";
-let country = "israel";
-let global_address = "i live " + addressStreet + addressNumber + country;
-
-console.log(global_address);
+// let secondstr = "hello Everyone, please say hello to the class";
+// let secondStrPosHello = secondstr.indexOf("hello"); 
+// console.log(secondStrPosHello) // 0
+// let adisu = "wow";
+// let adisus = "hey";
+// console.log(adisu);
 
 
- let birth = 1992;
- let year = 2021;
- let myBirthYear = "i will be in "  + (year - birth) + " in " + "2021";
- console.log( myBirthYear);
- let array = ["papaya" , "banana" ," orange"];
- console.log(array);
- console.log(array[0]);
- array[2] = "lemon";
- console.log(array);
-  let sampleArray = [
-    [1, 2, 5],
-    [7, 6, 10, 11, 12],
-    [3]
-];
+// let text = "welcome \weldone\  weljob";
+// console.log(text);
+// let Text = "Hello Everyone please say hello";
+// console.log(Text.substring(0,2));
 
-console.log(sampleArray);
-console.log(sampleArray[1][4]);
+// let addressNumber = 45;
+// let addressStreet = "allenby";
+// let country = "israel";
+// let global_address = "i live " + addressStreet + addressNumber + country;
+
+// console.log(global_address);
 
 
-let num=+5;
-console.log(num);
-console.log(typeof(sampleArray));
-console.log(sampleArray.length);
-console.log(sampleArray.push("5"));
-console.log(sampleArray);
-console.log(sampleArray.pop([1][2]));
-console.log(sampleArray);
-console.log(sampleArray.splice([1]));
+//  let birth = 1992;
+//  let year = 2021;
+//  let myBirthYear = "i will be in "  + (year - birth) + " in " + "2021";
+//  console.log( myBirthYear);
+//  let array = ["papaya" , "banana" ," orange"];
+//  console.log(array);
+//  console.log(array[0]);
+//  array[2] = "lemon";
+//  console.log(array);
+//   let sampleArray = [
+//     [1, 2, 5],
+//     [7, 6, 10, 11, 12],
+//     [3]
+// ];
 
-let pets = ["cat", "dog" ,"fish" ,"rabbit" , "cow"];
- console.log(pets[1]);
- let pet = "horse";
- console.log(pets.push(pet));
- console.log(pets);
+// console.log(sampleArray);
+// console.log(sampleArray[1][4]);
 
-//  excercise 1
 
-let fruits = ["banana","apples" , "oranges","blueberries"];
-console.log(fruits.splice([0
-]));
-// console.log(fruits.sort());
-// console.log(fruits);
-console.log( fruits);
-console.log(5+"34");
-console.log(5-"4");
-console.log(5%10);
-console.log("java"+"script");
-console.log(" "+ " ");
-console.log(" "+ 0);
+// let num=+5;
+// console.log(num);
+// console.log(typeof(sampleArray));
+// console.log(sampleArray.length);
+// console.log(sampleArray.push("5"));
+// console.log(sampleArray);
+// console.log(sampleArray.pop([1][2]));
+// console.log(sampleArray);
+// console.log(sampleArray.splice([1]));
 
-console.log(true + true);
+// let pets = ["cat", "dog" ,"fish" ,"rabbit" , "cow"];
+//  console.log(pets[1]);
+ 
+// console.log(pets[1]);
+//  let pet = "horse";
+//  console.log(pets.push(pet));
+//  console.log(pets);
+//  console.log(pets);
 
-console.log(true +false);
+// //  excercise 1
 
-console.log(false+ true);
-console.log(false-true);
-console.log(3-4);
-console.log("Bob" - "bill");
+// let fruits = ["banana","apples" , "oranges","blueberries"];
+// console.log(fruits.splice([0
+// ]));
+// // console.log(fruits.sort());
+// // console.log(fruits);
+// console.log( fruits);
 
-let me = ["my" , "favorite" , "color" ,"is" , "blue"];
- let result = "my" + " " + "favorite "  +  " color " + "is " +" blue";
- console.log(result)
 
- let name = "john";
- let lastname = "smith";
- let fullName = name  + lastname ;
- console.log(fullName.slice());
- console.log(name.slice(-2));
- console.log(lastname.slice(-2));
+// // exercise 1
+// console.log(5+"34");
+// console.log(5-"4");
+// console.log(5%10);
+// console.log("java"+"script");
+// console.log(" "+ " ");
+// console.log(" "+ 0);
 
- let firstName = "jo" + "hn";
- let = lastName = "sm" + "th";
- console.log(firstName);
- console.log(lastname);
+// console.log(true + true);
+
+// console.log(true +false);
+
+// console.log(false+ true);
+// console.log(false-true);
+// console.log(3-4);
+// console.log("Bob" - "bill");
+
+// let me = ["my" , "favorite" , "color" ,"is" , "blue"];
+//  let result = "my" + " " + "favorite "  +  " color " + "is " +" blue";
+//  console.log(result)
+//  // mixup
+//  let name = "john";
+//  let lastname = "smith";
+//  let fullName = name  + lastname ;
+//  console.log(fullName.slice());
+//  console.log(name.substring(0,1));
+//  console.log(lastname.slice(-2));
+
+//  let firstName = "jo" + "hn";
+//  let = lastName = "sm" + "th";
+//  console.log(firstName);
+//  console.log(lastname);
+
+
+ // 20 strings methods
+
+//  var stringOne = "freecodecamp is the best place to learn"
+//  var stringtwo = " frontend and backend development"
+
+// // chaAt()
+// console.log(stringOne.charAt(1))
+// console.log(stringOne.concat(stringtwo));
+// console.log(stringOne.includes("sh"))
+// console.log(stringOne.indexOf("learn"));
+// console.log(stringOne.endsWith("rn"));
+// console.log(stringOne.match(/end/g))
+// console.log(stringOne.repeat(4));
+// console.log(stringOne.search("to"));
+// console.log(stringOne.slice(1,2))
+// console.log(stringOne.toUpperCase().split(" "))
+// let stringThree = "  welcome   ";
+// console.log(stringThree.trim());
+
+// var sentences = "I love the movie Named Nemo!";
+// console.log(sentences.search("Nemo"));
+// console.log("I  found Nemo at" + " " + sentences.search("Nemo"))
+  
+   
+ // calculaator
 
 //  var calculator = prompt("Enter a number");
 //    var num1 = 6;
@@ -109,43 +142,305 @@ let me = ["my" , "favorite" , "color" ,"is" , "blue"];
 //    var sum = num1 +num2;
 //    alert(sum);
 
-// var sentences = prompt("user give full meaning of word");
-
-// var word = "i love the movie named Nemo";
-// var n = 
-//       (word.indexOf("nemo"));
+;
+      // evaluation
      
-     
-console.log(5>=1);
-console.log(0===1);
-console.log(4<=1);
-console.log(1!=1);
-console.log("A">"B")
-console.log("B"<"C")
-console.log("a">"A")
-console.log("b"<"A")
-console.log(true===false)
-console.log(true!=true)
+// console.log(5>=1);
+// console.log(0===1);
+// console.log(4<=1);
+// console.log(1!=1);
+// console.log("A">"B")
+// console.log("B"<"C")
+// console.log("a">"A")
+// console.log("b"<"A")
+// console.log(true===false)
+// console.log(true!=true)
+// evaluation(2);
+// let a = 34;
+// let b =21;
+// a = 2;
+//  let c = a+b;
+// console.log(a+b);
+// console.log(3+4+ "5");
 
-let a = 34;
-let b =21;
-a = 2;
- let c = a+b;
-console.log(a+b);
-console.log(3+4+ "5");
+//  let fruit = ["banana" , "Apples" , "oranges" , "blueberries"];
+//   console.log(fruit.shift())
+//   console.log(fruit)
+//   console.log(fruit.sort());
+//   var juice = "kiwi";
+//   console.log(fruit.push("kiwi"));
+//   console.log(fruit.reverse);
+//   console.log(fruit.splice(1));
+//   console.log(fruit);
+//   let  moreFruits = ["banana" , ["appples" , ["oranges"] ,"blueberies"]];
+  //  console.log(moreFruits[1][1]);
+    // conditionals
 
-var string = parseInt("Entera string numbers");
- var user1= 2;
- var user2 = 3;
- var sums =parseInt( 2, "3");
- console.log(sums);
- let fruit = ["banana" , "Apples" , "oranges" , "blueberries"];
-  console.log(fruit.shift())
-  console.log(fruit)
-  console.log(fruit.sort());
-  var juice = "kiwi";
-  console.log(fruit.push("kiwi"));
-  console.log(fruit);
-  console.log(fruit.replace())
+    // let age = 20;
+    //  if(age>18){
+    //    console.log("you are big man");
+    //  }
 
 
+//      let  Age = prompt("Enter your age");
+//      if(Age<18){
+//        alert("sorry ,you are too young to drive this car");
+
+//      }
+// else if(Age ==="18"){
+//   alert("congragulation on your first year of driving");
+// }
+
+// else (Age>18) 
+// {
+//   alert("power on enjoy the ride");
+// }
+
+// let a = 2 + 3;
+
+//     switch (a) {
+//       case 3:
+//         alert( 'Too larg ' );
+//         break;
+//       case 4:
+//         alert( 'too small' );
+//         break;
+//       case 5:
+//         alert( 'Too smaller' );
+//         break;
+//       default:
+//         alert( "I don't know such values" );
+//     }
+  
+// object
+
+// let person = {
+//   firstname: "john",
+//   lastname : "smith",
+//   age :34,
+//   eyeColor : "red"
+// };
+// console.log(person.eyeColor);
+
+
+
+// let data = {
+//   userName: "adisu",
+//   password: "12345",
+// }
+// console.log(data);
+//  var dataBase = [
+//   data = {
+//     userName: "adisu",
+//     password: "12345",
+//   }
+//  ]
+//  console.log(dataBase[0].userName);
+//  let stri = "Happy BirthDay";
+// let patt = /birthday/i;
+// let results = str.match(patt);
+// console.log(results); //returns true
+
+// if (result){
+//     console.log('Yes')
+// } else{
+//     console.log('No');
+
+// }
+
+// for(var x =0; x<=15; x++){
+//   if(x===0){
+//     console.log(x+ "is even");
+//   }
+//   else if(x%2 ===0){
+//     console.log(x+ "is even");
+//   }
+//   else{
+//     console.log(x+"is odd");
+//   }
+// };
+
+
+//ex.1 if/else statement
+
+// let x = 10;
+// let y = 7;
+
+// if(x>y){
+//   console.log("x is the biggest number");
+// }
+// else(x<y)
+// {
+
+//   console.log("y is the smallest number");
+
+// }
+
+
+// var newDog = "chihuahua";
+//  console.log(newDog.length)
+
+//  console.log(newDog.toUpperCase());
+
+//  var newDog = "chihuahua";
+//  if(newDog === "chihuahua"){
+//    console.log("i love chihuahuas");
+//  }
+//  else{
+//    console.log("i dont care i prefer cats");
+//  }
+
+
+ //var number = prompt("Enter a number");
+
+//  if(number=== 0){
+//    console.log( number + "is even");
+
+//  }
+// else if(number%2===0){
+//   console.log(number + " is even")
+  
+// }
+// else{
+//   console.log("is an odd");
+// }
+
+// let users = ["lea123", "princess45","cat&doglovers", "heloo@000"];
+// //console.log(users[0] + "is online")
+
+// if(users){
+//   console.log('no one is online');
+// }
+//  else if(users ==1){
+//   console.log(users[1] + "is online");
+
+//  }
+//  else if(users===2){console.log(users[0,1]+ "is online");
+
+//  }
+// var language = prompt("user language");
+//   // var language = ["French", "English","Hebrew"];
+
+//   if(language  =="French"){
+//     console.log("bomjour");
+//   }
+
+//   else if(language == "English"){
+//     console.log("Hello");
+//   }
+//   else if  (language ==
+//      "Hebrew"){
+//     console.log("shalom");
+//   }
+//   else{
+    
+    
+//    console.log ("01110011 01101111 ");
+//   }
+  // switch(language){
+  //   case 'French':
+  //     console.log("bonjour");
+      
+  //     case 'Egnlish':
+  //       console.log("hello");
+  //       break;
+  //       case 'Hebrew':
+  //         console.log("shalom");
+  //         break;
+  //         default:
+  //           console.log("01110011 01101111 01110010 01110010 01111001")  ;                       
+  // }
+
+//   var Grade = prompt("Enter value of Grade");
+
+//    if(Grade > 90){
+    
+//        console.log("A");
+//    }
+//      else if(Grade >=80 && Grade <=90){
+//      console.log("B");
+//    }
+//   else if(Grade >=70 && Grade <=80){
+//     console.log("C");
+//   }
+//     else (Grade < 70)
+//     {
+//       console.log("D");  
+//     }
+//  let number =15;
+//      for (number=0; number<15; number++){
+//     //   console.log(number[i]);
+//     // 
+
+//     if(number%2 === 0){
+//       console.log(number + "even");
+//     }
+   
+//     else{
+//       console.log(number + "odd");
+//     }
+//   }
+
+//   var verb = prompt("Enter user string");
+
+//     if(verb.length<3){
+//       console.log(verb);
+//     }
+//     else if(verb.slice(-3)== "ing"){
+//       console.log( verb + "ly")
+//     }
+//     else{
+//       console.log(verb + "ing");
+//     }
+//   let n = 2
+//   while(n>3)
+//   n++;
+//   console.log(n);
+
+//   let i = 0;
+// do {
+//   console.log("The number is " + i)
+//   i++;
+// }
+// while (i >10);
+
+// var names = ["john" ,"sarah", 23, "rudolf",34];
+  // for(let i= 0; i<names.length; i++){
+  //   console.log(names[i]);
+  // }
+  // for (let name of names){
+  //   if( typeof(name)!=="string"){
+  //     break;
+
+  //   }
+  //   console.log(name);
+  // }
+
+//   let names = ["john", "sarah", 23, "Rudolf", 34]
+// for (let i in names) {
+//     if (typeof (names[i]) === "number") {
+//         continue;
+//     }
+//     else if (typeof (names[i]) === "string") {
+//         if (names[i].charAt(0) != names[i].toUpperCase) {
+//             console.log(names[i].charAt(0).toUpperCase() + names[i].slice(1))
+//         }
+//     }
+// }
+// var names = ["john" ,"sarah", 23, "rudolf",34];
+// for(let i = 0; i<names.length; i++){
+//   //console.log(names[0][0].toUpperCase(1))
+//  // console.log(names[0][0].slice("j"))
+//   console.log(names[1].charAt([0][0]))
+// }
+var nam= "sarah";
+function sayHello(){
+  console.log(nam);
+  var numb = 34;
+  console.log(numb);
+}
+sayHello()
+sayHello();
+let na = "lea";
+ let greeting = `hello ${na}`;
+ console.log(greeting)
