b511c4df853d25bb3e580a77fd56a50bab857088
adisues2 <eshetu.adisu21@gmail.com>
2021-08-04 18:36:14 +0300

    week updated

---
+++ DOM/dom/week5day3/week_5day3.js
@@ -0,0 +1,52 @@
+
+
+function onload(){
+
+    
+    let banner = document.createElement('banner');
+    banner.textContent = 'the sales end in 1omin ';
+    document.body.appendChild(banner);
+    banner.style.background = "red";
+}  
+ setTimeout(onload ,1000);
+//   let counter = 10;
+//   let banner = document.createElement('banner');
+//     banner.textContent = 'the sales end in '+ counter + 'sec ';
+ 
+
+//     document.body.appendChild(banner);
+//     banner.style.background = "red";
+//  function onload(){
+//    clearInterval(10);
+//    banner.textContent = "done";
+//    return;
+    
+    
+// }  
+// let id =  setInterval(onload ,1000);
+
+
+// var intervalID = setInterval(myCallback, 5000, 'welcome', 'cool');
+
+// function myCallback(a, b)
+// {
+//  console.log(a);
+//  console.log(b);
+// }
+//myCallback();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
