4fcfaa5cde386fe2a3fb4f3ca6b2ed016b1ec471
adisues2 <eshetu.adisu21@gmail.com>
2021-08-21 11:41:43 +0300

    chalengeexercise

---
+++ pythonproject/PYTHONWEEK_7/minproject/challenge.py
@@ -0,0 +1,82 @@
+n = 10
+for i in range(1,6):
+    print(' '*n, end='')
+    print('*'*(i))
+    n-=1
+
+n = 10
+for i in range(1,6,2):
+    print(' '*n, end='')
+    print('*'*(i))
+    n-=1
+
+
+    my_list = [2, 24, 12, 354, 233]
+for i in range(len(my_list) - 1):
+    minimum = i
+    for j in range( i + 1, len(my_list)):
+        if(my_list[j] < my_list[minimum]):
+            minimum = j
+            if(minimum != i):
+                my_list[i], my_list[minimum] = my_list[minimum], my_list[i]
+print(my_list)
+
+sum = sum(my_list)
+print(sum)
+count= 0
+string = 'welcome thankyou'
+for i in  string:
+            if (i.isspace()):
+                count=count+1
+            print('space',count)
+
+# my_sum = ([1,5,4,2])
+# sum = sum(my_sum)
+# print(sum)
+     
+list = ([0,1,3,50])
+bG_N= max(list)
+print(bG_N)
+list_count= (['a','a','t','o'],'a')
+
+count = list_count[0].count('a')
+print("count of a",count)
+
+def calculate_upp(str):
+    s = {"upper_case":0 , "lower_case":0}
+    for u in str:
+        if u.isupper():
+            s['upper_case']+=1
+        elif u.islower():
+            s['lower_case']+=1
+        else:
+            pass
+        print('stringof',str)
+        print("string", s["upper_case"])
+        print("string", s["lower_case"])
+        calculate_upp('NetFlix')
+
+this_mono= ([7,6,5,5,2,0])
+
+print(this_mono)
+mono =sorted(this_mono)
+print(mono)
+num = ([2,3,3,3])  
+print(sorted(num))
+n = ([7,6,5,5,2,0])
+def arraySorted(n ):
+
+    array = []
+   
+    if (n == 1 or n == 0):
+        return 1
+    if (array[n - 1] < array[n - 2]):
+        return 0
+    return arraySorted(array, n - 1);
+print(n)
+ 
+lst = [1,23,34]
+str = ["banaana" ,"orange" ,"apple"]
+one_list = lst+str
+print(one_list)
+
