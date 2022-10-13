63d2cd5dd8e18a1af33d78ce75dea1e10d2e7f5e
adisu <adisues2@gmail.com>
2022-10-13 21:10:28 +0300

    files

---
+++ ap-ad-apps/routes.py
@@ -0,0 +1,27 @@
+# Imports, Car Model, handle_cars() method all truncated
+import requests
+@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_car(car_id):
+    car = CarsModel.query.get_or_404(car_id)
+
+    if request.method == 'GET':
+        response = {
+            "name": car.name,
+            "model": car.model,
+            "doors": car.doors
+        }
+        return {"message": "success", "car": response}
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        car.name = data['name']
+        car.model = data['model']
+        car.doors = data['doors']
+        db.session.add(car)
+        db.session.commit()
+        return {"message": f"car {car.name} successfully updated"}
+
+    elif request.method == 'DELETE':
+        db.session.delete(car)
+        db.session.commit()
+        return {"message": f"Car {car.name} successfully deleted."}
